fd714cceaddb058c641f03ea5e898a80aa2f574b
kimlimjustin <kimlimjustin@gmail.com>
2020-09-07 15:35:29 +0700

    Fixed bugs

--- tools/settings.py
+++ tools/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q4uzpz%%f=_c#yrf_8_*0=^^84*2s$-o9e9x3jog@$8632v=%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['basictools.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['basictools.herokuapp.com']
 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
